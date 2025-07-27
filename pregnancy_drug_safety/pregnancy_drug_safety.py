fb28201f96857922c07f49b9c980515c7c73d576
venkata sai pallavi pallapolu <143642448+vpallapolu@users.noreply.github.com>
2025-07-27 13:29:30 -0500

    Update pregnancy_drug_safety.py

--- pregnancy_drug_safety/pregnancy_drug_safety.py
+++ pregnancy_drug_safety/pregnancy_drug_safety.py
@@ -25,10 +25,10 @@ from tensorflow.keras.models import load_model
 model_path = os.path.join(os.path.dirname(__file__), "Tuned_Artificial_Neural_Network_Smote_Drug_Safety_Model.h5")
 model = load_model(model_path)
 base_dir = os.path.dirname(__file__)
-tfidf_vectorizer = load(os.path.join(os.path.dirname(__file__), "tfidf_vectorizer.pkl"))
-scaler = load(os.path.join(base_dir,"scaler.pkl"))
+tfidf_vectorizer = joblib.load(os.path.join(base_dir, "tfidf_vectorizer.pkl"))
+scaler = joblib.load(os.path.join(base_dir,"scaler.pkl"))
 
-onehot_encoder = load(os.path.join(base_dir,"fitted_onehot_encoder.pkl"))
+onehot_encoder = joblib.load(os.path.join(base_dir,"fitted_onehot_encoder.pkl"))
 saved_feature_names = np.load("onehot_feature_names.npy", allow_pickle=True)
 
 st.set_page_config(page_title="Pregnancy Drug Safety Predictor", layout="wide")
