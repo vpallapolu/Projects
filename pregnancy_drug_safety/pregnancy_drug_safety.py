922372ec1ad4445d4126a7abc3487601c1664051
venkata sai pallavi pallapolu <143642448+vpallapolu@users.noreply.github.com>
2025-07-27 13:32:09 -0500

    Update pregnancy_drug_safety.py

--- pregnancy_drug_safety/pregnancy_drug_safety.py
+++ pregnancy_drug_safety/pregnancy_drug_safety.py
@@ -29,7 +29,7 @@ tfidf_vectorizer = joblib.load(os.path.join(base_dir, "tfidf_vectorizer.pkl"))
 scaler = joblib.load(os.path.join(base_dir,"scaler.pkl"))
 
 onehot_encoder = joblib.load(os.path.join(base_dir,"fitted_onehot_encoder.pkl"))
-saved_feature_names = np.load("onehot_feature_names.npy", allow_pickle=True)
+saved_feature_names = np.load(os.path.join(base_dir,"onehot_feature_names.npy", allow_pickle=True))
 
 st.set_page_config(page_title="Pregnancy Drug Safety Predictor", layout="wide")
 st.image("Pregnant_drug.png", width=150)
